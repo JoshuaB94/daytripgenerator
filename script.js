2371154aab8a9d4af7ef2ad548d60aad27180922
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-22 16:16:20 -0500

    Established a function that logs a randomization of an array in script.js

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 "use strict";
 
+//These are the vars with assigned arrays that will be used to pull data from
 let randomDestinations = ['Puerto Rico','Hawaii','Florida','Alaska','Tatooine'];
 let randomRestaurants = ["Oga's Cantina","The Cheesecake Factory","L&L Diner","Shokunin","Ruby Tuesday"];
 let randomTransportation = ["Plane","Boat","DeLorean","Horse Carriage","Bus"];
 let randomEntertainment = ['Symphony','Broadway Play','Sight-Seeing','Baseball Game','Watch a Movie'];
+
+function randomizeDestination(destinationAnswer){
+    let randomDestination;
+    randomDestination = Math.floor(Math.random() * randomDestinations.length);
+    console.log(randomDestination)
+}
+
+randomizeDestination("Yes");
