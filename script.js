82ac9b7767d8aa7831d1595ffacebfab688e8837
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-22 14:36:02 -0500

    Added Cheesecake Factory as a Restaurant in script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 "use strict";
 
 let randomDestinations = ['Puerto Rico','Hawaii','Florida','Alaska','Tatooine'];
-let randomRestaurants = ["Oga's Cantina",  ]
+let randomRestaurants = ["Oga's Cantina","Cheesecake Factory" ]
