0b7167e36d7aca410b6e597652496e909894495a
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-22 14:52:49 -0500

    Declared var for Transportation mode in script.js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@
 
 let randomDestinations = ['Puerto Rico','Hawaii','Florida','Alaska','Tatooine'];
 let randomRestaurants = ["Oga's Cantina","The Cheesecake Factory","L&L Diner","Shokunin","Ruby Tuesday"];
+let randomTransportation = ["Plane","Boat","Electric Car","Horse Carriage","Bus"];
