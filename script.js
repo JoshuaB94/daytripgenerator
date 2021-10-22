39a1e01320e70724a33be9076af1d183071caf3c
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-22 15:02:09 -0500

    Declared var for Entertainment type in script.js

--- script.js
+++ script.js
@@ -2,4 +2,5 @@
 
 let randomDestinations = ['Puerto Rico','Hawaii','Florida','Alaska','Tatooine'];
 let randomRestaurants = ["Oga's Cantina","The Cheesecake Factory","L&L Diner","Shokunin","Ruby Tuesday"];
-let randomTransportation = ["Plane","Boat","Electric Car","Horse Carriage","Bus"];
+let randomTransportation = ["Plane","Boat","DeLorean","Horse Carriage","Bus"];
+let randomEntertainment = ['Symphony','Broadway Play','Sight-Seeing','Baseball Game','Watch a Movie'];
