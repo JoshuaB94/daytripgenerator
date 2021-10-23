0d1dcaad919f47b7ef346d321689116b8bc8b46e
Joshua Berrios <ixoyedesignstudio@gmail.com>
2021-10-23 03:02:12 -0500

    Renamed var declarations to less repitive names, created a function for the randomization of a Day Trip destination, and tested it out using an alert method.

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 "use strict";
 
 //These are the vars with assigned arrays that will be used to pull data from
-let randomDestinations = ['Puerto Rico','Hawaii','Florida','Alaska','Tatooine'];
-let randomRestaurants = ["Oga's Cantina","The Cheesecake Factory","L&L Diner","Shokunin","Ruby Tuesday"];
-let randomTransportation = ["Plane","Boat","DeLorean","Horse Carriage","Bus"];
-let randomEntertainment = ['Symphony','Broadway Play','Sight-Seeing','Baseball Game','Watch a Movie'];
+let mustGoDestination = ['Puerto Rico','Hawaii','Florida','Alaska','Tatooine'];
+let mustGoRestaurant = ["Oga's Cantina","The Cheesecake Factory","L&L Diner","Shokunin","Ruby Tuesday"];
+let modeOfTransportation = ["Plane","Boat","DeLorean","Horse Carriage","Bus"];
+let typeOfEntertainment = ['Symphony','Broadway Play','Sight-Seeing','Baseball Game','Watch a Movie'];
 
-function randomizeDestination(destinationAnswer){
-    let randomDestination;
-    randomDestination = Math.floor(Math.random() * randomDestinations.length);
-    console.log(randomDestination)
+
+function randomizeDestination(chosenDestination){
+    let destinationAnswer = Math.floor(Math.random() * mustGoDestination.length);
+    if(destinationAnswer === 0){
+        chosenDestination = mustGoDestination[0];
+    } else if(destinationAnswer === 1){
+        chosenDestination = mustGoDestination[1];
+    } else if(destinationAnswer === 2){
+        chosenDestination = mustGoDestination[2];
+    } else if(destinationAnswer === 3){
+        chosenDestination = mustGoDestination[3];
+    } else if(destinationAnswer === 4){
+        chosenDestination = mustGoDestination[4];
+    }
+    return chosenDestination;
 }
-
-randomizeDestination("Yes");
+alert(randomizeDestination());
